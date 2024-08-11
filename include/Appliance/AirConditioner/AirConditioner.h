192fb90992b4e2a3333ea05c1c2f7e403d8d750d
Michael <berg.michael1@gmail.com>
2024-08-11 11:47:39 -0500

    turn off power usage check

--- include/Appliance/AirConditioner/AirConditioner.h
+++ include/Appliance/AirConditioner/AirConditioner.h
@@ -22,8 +22,10 @@ class AirConditioner : public ApplianceBase {
  public:
   AirConditioner() : ApplianceBase(AIR_CONDITIONER) {}
   void m_setup() override;
-  void m_onIdle() override { this->m_getStatus();
-                      this->m_getPowerUsage(); }
+  void m_onIdle() override { 
+    this->m_getStatus();
+    // this->m_getPowerUsage(); 
+  }
   void control(const Control &control);
   void setPowerState(bool state);
   bool getPowerState() const { return this->m_mode != Mode::MODE_OFF; }
